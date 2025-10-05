3bb8f8bcf56cd9dcc84c80b3d9283ac977981cbf
albinjoby <albinjobyponnu@gmail.com>
2025-10-05 16:39:47 +0530

    add marquee

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/Button";
-import designExample1Image from '@/assets/images/design-example-1.png'
-import designExample2Image from '@/assets/images/design-example-2.png'
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designExample2Image from "@/assets/images/design-example-2.png";
 import Image from "next/image";
 import Pointer from "@/components/Pointer";
 
@@ -9,31 +9,44 @@ export default function Hero() {
         <section className="py-24 overflow-x-clip">
             <div className="container relative">
                 <div className="absolute -left-32 top-16 hidden lg:block">
-                    <Image src={designExample1Image} alt="design example"/>
+                    <Image src={designExample1Image} alt="design example" />
                 </div>
                 <div className="absolute -right-64 -top-16 hidden lg:block">
-                    <Image src={designExample2Image} alt="design example"/>
+                    <Image src={designExample2Image} alt="design example" />
                 </div>
                 <div className="absolute left-56 top-96 hidden lg:block">
-                    <Pointer name="Andria"/>
+                    <Pointer name="Andria" />
                 </div>
                 <div className="absolute right-80 -top-4 hidden lg:block">
-                    <Pointer name="Brian" color="red"/>
+                    <Pointer name="Brian" color="red" />
                 </div>
                 <div className="flex justify-center">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
                         ✨$7.5M seed round raised
                     </div>
                 </div>
-                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">Impactfull design, created effortlessly</h1>
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">
+                    Impactfull design, created effortlessly
+                </h1>
                 <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
                     Design tools shouldn&apos;t slow you down. Layers combines
                     powerful features with an intutive interface that keeps you
                     in your creative flow.
                 </p>
                 <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
-                    <input type="emial" placeholder="Enter you email" className="bg-transparent px-4 md:flex-1"/>
-                    <Button variant="primary" size="sm" className="whitespace-nowrap" type="submit">Sign Up</Button>
+                    <input
+                        type="emial"
+                        placeholder="Enter you email"
+                        className="bg-transparent px-4 md:flex-1 w-full"
+                    />
+                    <Button
+                        variant="primary"
+                        size="sm"
+                        className="whitespace-nowrap"
+                        type="submit"
+                    >
+                        Sign Up
+                    </Button>
                 </form>
             </div>
         </section>
