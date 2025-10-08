8b9baf8e38f2930ece814d39c133aa321740efb9
albinjoby <albinjobyponnu@gmail.com>
2025-10-08 22:22:30 +0530

    add avatar section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,11 @@
+import Tag from "@/components/Tag";
+import FeatureCard from "@/components/FeatureCard";
+import avatar1 from "@/assets/images/avatar-ashwin-santiago.jpg"
+import avatar2 from "@/assets/images/avatar-lula-meyers.jpg"
+import avatar3 from "@/assets/images/avatar-florence-shaw.jpg"
+import Image from "next/image";
+import Avatar from "@/components/Avatar";
+
 const features = [
     "Asset Library",
     "Code Preview",
@@ -9,5 +17,71 @@ const features = [
 ];
 
 export default function Features() {
-    return <div>Features</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                    <Tag>Features</Tag>
+                </div>
+                <h2 className="text-6xl font-medium text-center mt-6">
+                    Where power meets{" "}
+                    <span className="text-lime-400">simplicity</span>
+                </h2>
+                <div className="mt-12 grid grid-cols-1 gap-8">
+                    <FeatureCard
+                        title="Real-time Collaboration"
+                        description=" Work together seemlessly with conflict-free teamediting"
+                    >
+                        <div className="aspect-video flex items-center justify-center">
+                            <Avatar className="z-40">
+                                <Image
+                                    className="rounded-full"
+                                    src={avatar1}
+                                    alt="avatar1"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-indigo-500 z-30">
+                                <Image
+                                    className="rounded-full"
+                                    src={avatar2}
+                                    alt="avatar2"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-amber-500 z-20">
+                                <Image
+                                    className="rounded-full"
+                                    src={avatar3}
+                                    alt="avatar3"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-transparent">
+                                <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1">
+                                    {Array.from({length: 3}).map((_, idx) => (
+                                        <span key={idx} className="size-1.5 rounded-full bg-white inline-flex"></span>
+                                    ))}
+                                </div>
+                            </Avatar>
+                        </div>
+                    </FeatureCard>
+                    <FeatureCard
+                        title="Interactive Prototyping"
+                        description=" Engage you clients with prototypes what react with user actions"
+                    ></FeatureCard>
+                    <FeatureCard
+                        title="Keyboard Quick Actions"
+                        description="Powerful commands to help you create designs
+                                more quickly"
+                    ></FeatureCard>
+                </div>
+                <div>
+                    {features.map((feature, idx) => (
+                        <div key={idx}>
+                            <span></span>
+                            <span>{feature}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
